b17f3002715f7870e02269c0a3012f0fc24187d1
DavyJonec <dranik_ne_blin@mail.ru>
2021-10-31 13:01:26 +0300

    Commit #6 and #7

--- src/main/java/ru/geekbrains/Homework5/Recursion/MainBackpack.java
+++ src/main/java/ru/geekbrains/Homework5/Recursion/MainBackpack.java
@@ -7,11 +7,11 @@ public class MainBackpack {
     public static void main(String[] args) {
 
     List<Item> items = new ArrayList<>();
-        items.add(new Item("Book",   1, 600)); // +
-        items.add(new Item("Knife", 2, 5000)); // +
-        items.add(new Item("Bow", 4, 4500));  // -
-        items.add(new Item("Supplies", 6, 8000)); // +
-        items.add(new Item("Crossbow ", 3, 7500)); // -
+        items.add(new Item("Book",   1, 600));
+        items.add(new Item("Knife", 2, 5000));
+        items.add(new Item("Bow", 4, 4500));
+        items.add(new Item("Supplies", 6, 8000));
+        items.add(new Item("Crossbow ", 3, 7500));
 
 
     Backpack backpack = new Backpack(8);
